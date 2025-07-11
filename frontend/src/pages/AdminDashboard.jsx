99b731ae746cd05ea4b608d12c43d96bd0309a01
Guruprasath <guruprasath1302@gmail.com>
2025-07-11 04:32:58 +0000

    updating the css

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -3,7 +3,18 @@
 import React from 'react';
 
 const AdminDashboard = () => {
-  return <div>Admin Dashboard</div>;
+  return( 
+  <div className='text-light-lavender text-5xl '>
+    <h1 className='h-2/10 text-5xl p-2'>
+      ADMIN PAGE
+    </h1>
+    <div className='h-8/10 min-h- flex'>
+      <div className='sideBar w-2/10 min-w-min border-2 border-amber-300'>vfsdv s</div>
+      <div className='content w-8/10 min-w-min border-2 border-amber-300'>vdsvsv</div>
+
+    </div>
+
+  </div>);
 };
 
 export default AdminDashboard;
