fdef418301d4289ec7725b368f2bb20429b2e8a5
Guruprasath <guruprasath1302@gmail.com>
2025-07-11 03:39:58 +0000

    running on the gitpod frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["5173-gru13-iadst-t0gmxcpaxyy.ws-us120.gitpod.io"]
+  },
 })
