b680acb9e7863638d9be7e7d8d929fc41de6fe40
Guruprasath <guruprasath1302@gmail.com>
2025-07-11 04:00:34 +0000

    tailwind added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: ["5173-gru13-iadst-t0gmxcpaxyy.ws-us120.gitpod.io"]
   },
